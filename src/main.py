9d7828ba29b416a5be48b9b8c749aa9cd484ea1f
WeetHet <stas.ale66@gmail.com>
2024-07-19 11:08:55 +0300

    Work on mcontained, add support for non file logging

--- src/main.py
+++ src/main.py
@@ -15,8 +15,12 @@ from verifier import Verifier
 
 if not os.path.exists("log"):
     os.mkdir("log")
+
+no_file = os.environ.get("NOFILE")
+logging_file = "log/llm.log" if no_file is None or no_file == "0" else None
 logging.basicConfig(
-    level=os.environ.get("PYLOG_LEVEL", "INFO").upper(), filename="log/llm.log"
+    level=os.environ.get("PYLOG_LEVEL", "INFO").upper(),
+    filename=logging_file,
 )
 logger = logging.getLogger(__name__)
 
