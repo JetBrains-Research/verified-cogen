6b6bd5ae2313ec24078da0f5b9604ce299478b1f
WeetHet <stas.ale66@gmail.com>
2025-01-08 16:39:34 +0200

    Add step by step flush runner to the GUI

--- flake.nix
+++ flake.nix
@@ -1,21 +1,29 @@
 {
   inputs = {
-    flakelight.url = "github:nix-community/flakelight";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flakelight = {
+      url = "github:nix-community/flakelight";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { flakelight, ... }@inputs:
+  outputs =
+    { flakelight, ... }@inputs:
     flakelight ./. {
       inherit inputs;
-
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      devShell = pkgs: {
-        packages = pkgs: (with pkgs; [
-          poetry
-          dafny
-        ]) ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
-          AppKit
-          pkgs.libiconv
-        ]));
-      };
-      formatter = pkgs: pkgs.nixpkgs-fmt;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      devShell.packages = pkgs: [
+        pkgs.poetry
+        (pkgs.dafny.override {
+          z3 = pkgs.z3.override {
+            stdenv = pkgs.llvmPackages_16.stdenv;
+          };
+        })
+      ];
+      formatter = pkgs: pkgs.nixfmt-rfc-style;
     };
 }
