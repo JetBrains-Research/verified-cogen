fccd5d5af98c0796b030ed5c41b66555da72ad8c
WeetHet <stas.ale66@gmail.com>
2025-01-30 16:07:32 +0400

    Generic LLM done

--- verified_cogen/llm/llm.py
+++ verified_cogen/llm/llm.py
@@ -68,103 +68,39 @@ class LLM(ABC):
     def wipe_temporary(self) -> None: ...
 
 
-class LLMGrazie(LLM):
+class LLMGeneric(LLM):
     def __init__(
-        self,
-        grazie_token: str,
-        profile: str,
-        prompt_dir: str,
-        temperature: float,
-        system_prompt: Optional[str] = None,
+        self, prompt_dir: str, temperature: float, system_prompt: Optional[str] = None
     ):
         super().__init__(prompt_dir)
-        self.grazie = GrazieApiGatewayClient(
-            url=GrazieApiGatewayUrls.STAGING,
-            grazie_jwt_token=grazie_token,
-            auth_type=AuthType.USER,
-        )
-        self.profile = Profile.get_by_name(profile)
         self.prompt_dir = prompt_dir
-        self.is_gpt = "gpt" in self.profile.name
-        self.user_prompts: list[str] = []
-        self.is_user_prompt_temporary: list[bool] = []
-        self.responses: list[str] = []
-        self.is_response_temporary: list[bool] = []
-        self.had_errors = False
         self.temperature = temperature
         self.system_prompt = (
             system_prompt if system_prompt else prompts.sys_prompt(self.prompt_dir)
         )
+        self.messages: list[dict[str, str]] = []
+        self.message_is_temporary: list[bool] = []
 
     def add_user_prompt(self, prompt: str, temporary: bool = False):
-        self.user_prompts.append(prompt)
-        self.is_user_prompt_temporary.append(temporary)
+        self.messages.append({"role": "user", "content": prompt})
+        self.message_is_temporary.append(temporary)
 
     def add_response(self, response: str, temporary: bool = False):
-        self.responses.append(response)
-        self.is_response_temporary.append(temporary)
+        self.messages.append({"role": "assistant", "content": response})
+        self.message_is_temporary.append(temporary)
 
     def wipe_all(self):
-        self.user_prompts = []
-        self.is_user_prompt_temporary = []
-        self.responses = []
-        self.is_response_temporary = []
+        self.messages = []
+        self.message_is_temporary = []
 
     def wipe_temporary(self):
-        self.user_prompts = [
-            prompt
-            for prompt, temporary in zip(
-                self.user_prompts, self.is_user_prompt_temporary
-            )
-            if not temporary
+        self.messages = [
+            m for m, t in zip(self.messages, self.message_is_temporary) if not t
         ]
-        self.responses = [
-            response
-            for response, temporary in zip(self.responses, self.is_response_temporary)
-            if not temporary
-        ]
-        self.is_user_prompt_temporary = [False] * len(self.user_prompts)
-        self.is_response_temporary = [False] * len(self.responses)
+        self.message_is_temporary = [False] * len(self.messages)
 
-    def _request(
-        self, temperature: Optional[float] = None, tries: int = 5
-    ) -> ChatResponse:
-        if tries == 0:
-            raise Exception("Exhausted tries to get response from Grazie API")
-        if temperature is None:
-            temperature = self.temperature
-        prompt = ChatPrompt().add_system(self.system_prompt)
-        current_prompt_user = 0
-        current_response = 0
-        while current_prompt_user < len(self.user_prompts) or current_response < len(
-            self.responses
-        ):
-            if current_prompt_user < len(self.user_prompts):
-                prompt = prompt.add_user(self.user_prompts[current_prompt_user])
-                current_prompt_user += 1
-            if current_response < len(self.responses):
-                prompt = prompt.add_assistant(self.responses[current_response])
-                current_response += 1
-
-        try:
-            return self.grazie.chat(
-                chat=prompt,
-                profile=self.profile,
-                parameters={
-                    LLMParameters.Temperature: Parameters.FloatValue(temperature)
-                },
-            )
-        except (RemoteDisconnected, RequestFailedException):
-            logger.warning("Grazie API is down, retrying...")
-            return self._request(temperature, tries - 1)
-        except Exception as e:
-            self.dump_history(Path("err_dump.txt"))
-            raise e
-
-    def make_request(self) -> str:
-        response = self._request().content
-        self.add_response(response)
-        return extract_code_from_llm_output(response)
+    @abstractmethod
+    def make_request(self) -> str: ...
 
     def produce(self, prg: str) -> str:
         self.add_user_prompt(
@@ -193,12 +129,8 @@ class LLMGrazie(LLM):
         return self.make_request()
 
     def ask_for_fixed(self, err: str) -> str:
-        prompt = (
-            prompts.ask_for_fixed_had_errors_prompt(self.prompt_dir)
-            if self.had_errors
-            else prompts.ask_for_fixed_prompt(self.prompt_dir)
-        )
-        self.add_user_prompt(prompt.format(error=err))
+        prompt = prompts.ask_for_fixed_prompt(self.prompt_dir).format(error=err)
+        self.add_user_prompt(prompt)
         return self.make_request()
 
     def ask_for_timeout(self) -> str:
@@ -207,98 +139,88 @@ class LLMGrazie(LLM):
 
     def dump_history(self, file: Path):
         with open(file, "w") as f:
-            current_prompt_user = 0
-            current_response = 0
-            while current_prompt_user < len(
-                self.user_prompts
-            ) or current_response < len(self.responses):
-                if current_prompt_user < len(self.user_prompts):
-                    user_prompt = self.user_prompts[current_prompt_user]
-                    f.write(f"User: {user_prompt}\n\n")
-                    current_prompt_user += 1
-                if current_response < len(self.responses):
-                    f.write(f"LLM: {self.responses[current_response]}\n\n")
-                    current_response += 1
-
-
-class LLMOllama(LLM):
-    def __init__(self, model: str, prompt_dir: str, temperature: float = 0.7):
-        super().__init__(prompt_dir)
-        self.model = model
-        self.prompt_dir = prompt_dir
-        self.temperature = temperature
-        self.messages: list[dict[str, str]] = []
-        self.message_is_temporary: list[bool] = []
-        self.system_prompt = prompts.sys_prompt(self.prompt_dir)
-        self._add_system_message(self.system_prompt)
-
-    def _add_system_message(self, content: str):
-        self.messages.append({"role": "system", "content": content})
-        self.message_is_temporary.append(False)
-
-    def add_user_prompt(self, prompt: str, temporary: bool = False):
-        self.messages.append({"role": "user", "content": prompt})
-        self.message_is_temporary.append(temporary)
-
-    def add_response(self, response: str, temporary: bool = False):
-        self.messages.append({"role": "assistant", "content": response})
-        self.message_is_temporary.append(temporary)
-
-    def wipe_all(self) -> None:
-        self.messages = []
-        self.message_is_temporary = []
-
-    def wipe_temporary(self) -> None:
-        self.messages = [
-            m for m, t in zip(self.messages, self.message_is_temporary) if not t
-        ]
-        self.message_is_temporary = [False] * len(self.messages)
-
-    def make_request(self) -> str:
-        response = ollama.chat(model=self.model, messages=self.messages)  # type: ignore
-        assert isinstance(response, ollama.ChatResponse)
-        content = response["message"]["content"]
-        self.add_response(content)
-        return extract_code_from_llm_output(content)
-
-    def produce(self, prg: str) -> str:
-        prompt = prompts.produce_prompt(self.prompt_dir).format(program=prg)
-        self.add_user_prompt(prompt)
-        return self.make_request()
-
-    def add(self, prg: str, checks: str, function: Optional[str] = None) -> str:
-        prompt = prompts.add_prompt(self.prompt_dir).format(program=prg, checks=checks)
-        if "{function}" in prompt and function is not None:
-            prompt = prompt.replace("{function}", function)
-        self.add_user_prompt(prompt)
-        return self.make_request()
-
-    def rewrite(
-        self,
-        prg: str,
-        text_description: Optional[str] = None,
-        additional_prompt: str = "",
-    ) -> str:
-        result = prompts.rewrite_prompt(self.prompt_dir).replace("{program}", prg)
-        if text_description is not None and "{text_description}" in result:
-            result = result.replace("{text_description}", text_description)
-        result = result + "\n" + additional_prompt
-        self.add_response(result)
-        return self.make_request()
-
-    def ask_for_fixed(self, err: str) -> str:
-        prompt = prompts.ask_for_fixed_prompt(self.prompt_dir).format(error=err)
-        self.add_user_prompt(prompt)
-        return self.make_request()
-
-    def ask_for_timeout(self) -> str:
-        prompt = prompts.ask_for_timeout_prompt(self.prompt_dir)
-        self.add_user_prompt(prompt)
-        return self.make_request()
-
-    def dump_history(self, file: Path):
-        with open(file, "w") as f:
             for message in self.messages:
                 role = message["role"].capitalize()
                 content = message["content"]
                 f.write(f"{role}: {content}\n\n")
+
+
+class LLMGrazie(LLMGeneric):
+    def __init__(
+        self,
+        grazie_token: str,
+        profile: str,
+        prompt_dir: str,
+        temperature: float,
+        system_prompt: Optional[str] = None,
+    ):
+        super().__init__(prompt_dir, temperature, system_prompt)
+        self.grazie = GrazieApiGatewayClient(
+            url=GrazieApiGatewayUrls.STAGING,
+            grazie_jwt_token=grazie_token,
+            auth_type=AuthType.USER,
+        )
+        self.profile = Profile.get_by_name(profile)
+        self.is_gpt = "gpt" in self.profile.name
+        self.had_errors = False
+        self.messages.append({"role": "system", "content": self.system_prompt})
+        self.message_is_temporary.append(False)
+
+    def _request(
+        self, temperature: Optional[float] = None, tries: int = 5
+    ) -> ChatResponse:
+        if tries == 0:
+            raise Exception("Exhausted tries to get response from Grazie API")
+        if temperature is None:
+            temperature = self.temperature
+
+        prompt = ChatPrompt()
+        for message in self.messages:
+            match message["role"]:
+                case "system":
+                    prompt = prompt.add_system(message["content"])
+                case "user":
+                    prompt = prompt.add_user(message["content"])
+                case "assistant":
+                    prompt = prompt.add_assistant(message["content"])
+                case _:
+                    raise ValueError(f"Unknown role: {message['role']}")
+
+        try:
+            return self.grazie.chat(
+                chat=prompt,
+                profile=self.profile,
+                parameters={
+                    LLMParameters.Temperature: Parameters.FloatValue(temperature)
+                },
+            )
+        except (RemoteDisconnected, RequestFailedException):
+            logger.warning("Grazie API is down, retrying...")
+            return self._request(temperature, tries - 1)
+        except Exception as e:
+            self.dump_history(Path("err_dump.txt"))
+            raise e
+
+    def make_request(self) -> str:
+        response = self._request().content
+        self.add_response(response)
+        return extract_code_from_llm_output(response)
+
+
+class LLMOllama(LLMGeneric):
+    def __init__(self, model: str, prompt_dir: str, temperature: float = 0.7):
+        super().__init__(prompt_dir, temperature)
+        self.model = model
+        self.messages.append({"role": "system", "content": self.system_prompt})
+        self.message_is_temporary.append(False)
+
+    def make_request(self) -> str:
+        response = ollama.chat(  # type: ignore
+            model=self.model,
+            messages=self.messages,
+            options={"temperature": self.temperature},
+        )
+        assert isinstance(response, ollama.ChatResponse)
+        content = response["message"]["content"]
+        self.add_response(content)
+        return extract_code_from_llm_output(content)
